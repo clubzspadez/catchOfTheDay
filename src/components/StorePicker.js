3aad44b9443e6191a7fe6e58bdcfa5a9193666d6
Jonathan Rodriguez <clubzspadez@gmail.com>
2017-12-26 21:51:32 -0500

    creating components and first form

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,7 +2,14 @@ import React from 'react';
 
 class StorePicker extends React.Component {
   render(){
-    return <p>HELLO MATE</p>
+    return (
+    		<form className="store-selector">
+    			{ */ COMMENTS*/}
+				<h2>Please enter a store</h2>
+				<input type="text"  required placeholder="Store Name" />
+				<button type="submit">Visit Store</button>
+    		</form>
+    	)
   }
 
 }
