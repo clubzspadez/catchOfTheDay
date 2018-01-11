2a6386e759927756754c4bc6c6503f5083fad945
Jonathan Rodriguez <clubzspadez@gmail.com>
2018-01-11 05:09:15 -0500

    displaying state with jsx

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,9 @@ import React from 'react';
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
-import sampleFishes from '../sample-fishes.js'
+import sampleFishes from '../sample-fishes.js';
+import Fish from  './Fish';
+
 
 class App extends React.Component {
   constructor(){
@@ -21,15 +23,21 @@ class App extends React.Component {
     this.setState({fishes});
   }
   loadSamples(){
-    this.setState = {
+    this.setState ({
       fishes: sampleFishes
-    }
+    });
   }
   render(){ 
     return (
     		<div className="catch-of-the-day">
-          <div className="menu"> 
+          <div className="menu">
             <Header tagline="Fresh Fish"/>
+            <ul className="list-of-fishes">
+              {
+                Object.keys(this.state.fishes)
+                .map(key => <Fish key={key} details={this.state.fishes[key]} />)
+              }
+            </ul>
           </div>
           <Order />
           <Inventory addFish={this.addFish} loadSamples={this.loadSamples} />
