18643906b9ec110249d650b24e5a2f49323db1a1
Jonathan Rodriguez <clubzspadez@gmail.com>
2018-01-18 08:03:40 -0500

    working with inventory to store and modify current samplefishes within our app

--- src/components/App.js
+++ src/components/App.js
@@ -81,7 +81,7 @@ class App extends React.Component {
             </ul>
           </div>
           <Order fishes={this.state.fishes} order={this.state.order} params={this.state.params}/>
-          <Inventory addFish={this.addFish} loadSamples={this.loadSamples} />
+          <Inventory addFish={this.addFish} loadSamples={this.loadSamples}  fishes={this.state.fishes} />
         </div>
     	)
   }
